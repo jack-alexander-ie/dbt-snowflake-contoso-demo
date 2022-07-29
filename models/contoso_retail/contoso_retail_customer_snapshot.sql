8121b0a530344f2720671166035cc893c6c0931e
Jack Mason <jack.mason@fivetran.com>
2022-07-29 15:57:20 +0100

    transforms: Adding the first transforms for Contoso

---
+++ models/contoso_retail/contoso_retail_customer_snapshot.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='table') }}
+
+with source_data as (
+    SELECT 
+        CASE 
+            WHEN GENDER = 'M' THEN 'M' 
+            WHEN GENDER = 'F' THEN 'F' 
+            WHEN GENDER IS NULL THEN 'Unspecified'
+            ELSE 'Mx'
+        END AS GENDER,
+        ROUND(AVG(DATEDIFF('year', BIRTHDATE, CURRENT_TIMESTAMP))) AS AVERAGE_AGE,
+        ROUND(AVG(YEARLYINCOME), 3) AS AVERAGE_INCOME,
+        ROUND(AVG(TOTALCHILDREN), 2) AS AVERAGE_NUM_CHILDREN,
+        ROUND(AVG(DATEDIFF('year', BIRTHDATE, DATEFIRSTPURCHASE))) AS AVERAGE_AGE_AT_FIRST_PURCHASE
+    FROM "JACKM_CONTOSO"."CONTOSO_RETAIL_DW_DBO"."DIMCUSTOMER"
+    GROUP BY GENDER;
+)
+
+select *
+from source_data
